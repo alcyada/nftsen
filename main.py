0636c416ee15cea2f18f815f64a60d24a5e715f4
alcyada <herokuwar4@gmail.com>
2022-05-02 19:37:37 +0000

    hi

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ driver.get("https://hotpot.ai/art-maker?s=nft-generator")
 ids = driver.find_element_by_tag_name('textarea')
 ids.click
 ids.send_keys("some text")
-sleep(5)
 ele=driver.find_element_by_tag_name('span')
 ele.click
 sleep(120)
 ele2 = driver.find_element_by_class_name('imageBox.targetBox')
 ele2.click
 sleep(20)
-ele3 = driver.find_element_by_class_name('imageBox')
+ele3 = driver.find_element_by_tag_name('img')
 ele3.click
+file.write(el3.screenshot_as_png)
