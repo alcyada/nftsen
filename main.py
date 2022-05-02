77947769d18ea3339b65c79f49d5161874438653
alcyada <herokuwar4@gmail.com>
2022-05-02 19:07:44 +0000

    hi

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from time import sleep
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver import FirefoxOptions
@@ -6,6 +7,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 opts = FirefoxOptions()
 opts.add_argument("--headless")
 driver = webdriver.Firefox(service=Service("/usr/bin/geckodriver"), options=opts)
-driver.get("http://www.python.org")
-ids = driver.find_element_by_class_name('option.seedTextBox')
-print(ids)
+driver.get("https://hotpot.ai/art-maker?s=nft-generator")
+ids = driver.find_element_by_tag_name('textarea')
+ids.click
+ids.send_keys("some text")
+sleep(5)
+ele=driver.find_element_by_tag_name('span')
+ele.click
+sleep(120)
+ele2 = driver.find_element_by_class_name('imageBox.targetBox')
+ele2.click
+sleep(20)
+ele3 = driver.find_element_by_class_name('imageBox')
+ele3.click
